8ca9849d268b1cf7ba23bdcd65b091c461ea32de
Justin Flick <jflick84@gmail.com>
2018-02-10 13:09:02 -0500

    RENDERING MINIMUM CHAR CREATION PAGE

---
+++ controllers/authcontroller.js
@@ -0,0 +1,35 @@
+var exports = module.exports = {}
+ 
+exports.signup = function(req, res) {
+ 
+    res.render('signup');
+ 
+}
+
+exports.signin = function(req, res) {
+ 
+    res.render('signin');
+ 
+}
+
+exports.characterCreation = function(req, res) {
+ 
+    res.render('characterCreation');
+ 
+}
+
+exports.dashboard = function(req, res) {
+ 
+    res.render('dashboard');
+ 
+}
+
+exports.logout = function(req, res) {
+ 
+    req.session.destroy(function(err) {
+ 
+        res.redirect('/');
+ 
+    });
+ 
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express')
+var app = express()
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
+  
+// For BodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("public"));
+  
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // Session secret
+app.use(passport.initialize());
+app.use(passport.session()); // Persistent login sessions
+  
+// For Handlebars
+// app.set('views', __dirname + '/views')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Index route
+// app.get('/', function(req, res) {
+//     res.send('Welcome to Passport with Sequelize');
+// });
+ 
+// Models
+var models = require("./models");
+ 
+// Routes 
+var authRoute = require('./routes/auth.js')(app,passport);
+ 
+// Load passport strategies 
+require('./config/passport/passport.js')(passport, models.user);
+ 
+// Sync Database 
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+ 
+app.listen(5000, function(err) {
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+});
