56818729a1912149a3d714466838ce9602f5158b
gambilldw <gambilldw@gmail.com>
2018-02-13 19:22:02 -0500

    geolocate n stuff

---
+++ Character Sheet.js
@@ -0,0 +1,71 @@
+//Character Name
+var cnm = "";
+
+//Character Class
+var ccl = [sol, grd, rng, inv, vgb];
+    //Soldier
+    //Guardian
+    //Ranger
+    //Invoker
+    //Vagabond
+
+//Character Race
+var crc = [hum, elf, dwr, hlf];
+    //Human
+    //Elf
+    //Dwarf
+    //Halfling
+
+//Character Element
+var cel = [er, wn, fr, wt, mt];
+    //Earth
+    //Wind
+    //Fire
+    //Water
+    //Metal
+
+//Character Ethereal
+var cet = [li, dr, vo, mn, wl];
+    //Light
+    //Dark
+    //Void
+    //Mind
+    //Wild
+
+//Strength
+var str = 4;
+//Defense
+var def = 4;
+//Agility
+var agi = 4;
+//Intellect
+var int = 4;
+//Luck
+var lck = 4;
+
+//Health
+var hp = (20);
+//Magic
+var mp = (10+int);
+//Energy
+var ep = (2+agi);
+//Fate
+var ft = (lck-2);
+
+//Equipment
+var weap
+var armr
+var trnk
+var potn
+
+var inv
+
+
+//item table 1
+var quality = [plain, ];
+
+//item table 2
+var attribute = [];
+//item table 3
+var type = [];
+
