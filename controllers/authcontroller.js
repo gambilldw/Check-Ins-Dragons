21984bcbf02b8e7dd21b4a30f8c19e3883c7245f
Justin Flick <jflick84@gmail.com>
2018-02-16 20:31:28 -0500

    updating user authentication, front end display

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,38 +1,26 @@
 var exports = module.exports = {}
- 
-exports.signup = function(req, res) {
- 
-    res.render('signup');
- 
-}
-
-exports.signin = function(req, res) {
- 
-    res.render('signin');
- 
-}
 
 exports.login = function(req, res) {
- 
+	
     res.render('login');
  
 }
 
-exports.characterCreation = function(req, res) {
+exports.signup = function(req, res) {
+ 	
+    res.render('signup');
  
-    res.render('characterCreation');
+}
+
+exports.characterCreation = function(req, res) {
+
+    res.render('characterCreation', { username: req.user.firstname });
  
 }
 
 exports.createUser = function(req, res) {
  
     res.render('createUser');
- 
-}
-
-exports.dashboard = function(req, res) {
- 
-    res.render('dashboard');
  
 }
 
