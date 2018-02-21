894d1c15f1508cd294e66b929c3e68516670ff95
Preston Ross <prestonross77@gmail.com>
2018-02-21 12:29:47 -0500

    contact page added

---
+++ contact/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Contact from "./components/Contact";
+
+const App = () => (
+  <div>
+    <Contact />
+  </div>
+);
+
+export default App;
---
+++ contact/src/components/Contact.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "../styles/Contact.css";
+
+const Contact = () => (
+
+    <body>
+
+        <div className="container-fluid">
+
+            <div className="row">
+
+                <div className="col-6">
+
+                    <h1 className="title">Contact Page</h1>
+
+                </div>
+
+            </div>
+
+            <div className="row">
+
+                <div className="col-1">
+
+                    <div className="memberContainer">
+
+                        <h2>Preston Ross</h2>
+
+                        <p><strong>Project Role:</strong> Front-End Developer</p>
+
+                        <p><strong>GitHub:</strong> <a href="https://github.com/PRoss7">https://github.com/PRoss7</a> </p>
+
+                    </div>
+
+                </div>
+
+                <div className="col-1">
+
+                    <div className="memberContainer">
+
+                        <h2>Imad Baber</h2>
+
+                        <p><strong>Project Role: </strong></p>
+
+                        <p><strong>GitHub:</strong> <a href=""></a> </p>
+
+                    </div>
+
+                </div>
+
+                <div className="col-1">
+
+                    <div className="memberContainer">
+
+                        <h2>Derek Gambill</h2>
+
+                        <p><strong>Project Role:</strong> API and Game Logic Developer</p>
+
+                        <p><strong>GitHub:</strong> <a href="https://github.com/gambilldw">https://github.com/gambilldw</a> </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="row">
+
+                <div className="col-1">
+
+                    <div className="memberContainer1">
+
+                        <h2>Justin Flick</h2>
+
+                        <p><strong>Project Role:</strong> Database Management and User Authentication</p>
+
+                        <p><strong>GitHub:</strong> <a href="github.com/j-flick">github.com/j-flick</a> </p>
+
+                    </div>
+
+                </div>
+
+                <div className="col-1">
+
+                    <div className="memberContainer1">
+
+                        <h2>Dominic Fumbanks</h2>
+
+                        <p><strong>Project Role:</strong> Front-End Developer</p>
+
+                        <p><strong>GitHub:</strong> <a href="https://github.com/fumbanksd">https://github.com/fumbanksd</a> </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="row">
+
+                <div className="col-6">
+
+                    <div className="locationContainer">
+
+                        <h2>Headquarters</h2>
+
+                        <p>UNCC Center City Campus</p>
+                        <p>320 E 9th St</p>
+                        <p>Charlotte, NC 28202</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
+);
+
+export default Contact;
